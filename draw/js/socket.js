66f478180dd6f9b5f2a31d5a541d5fa1a2543a7d
HedgeDA <faraon.da@gmail.com>
2019-02-10 01:28:36 +0600

    ...

--- draw/js/socket.js
+++ draw/js/socket.js
@@ -1,1 +1,29 @@
 'use strict';
+
+const wsDraw = new WebSocket('ws://neto-api.herokuapp.com/draw');
+
+function drawOpen(event) {
+  console.log('open');
+  console.log(event);
+  console.log(event.data);
+}
+
+function drawMessage(event) {
+  console.log('message');
+  console.log(event);
+  console.log(event.data);
+}
+
+function editorUpdate(event) {
+  const image = event.canvas.getContext('2d').getImageData(0, 0, event.canvas.width, event.canvas.height);
+  const binary = Uint8Array.from(image.data);
+  wsDraw.send(binary.buffer);
+}
+
+function init() {
+  wsDraw.addEventListener('open', drawOpen);
+  wsDraw.addEventListener('message', drawMessage);
+  window.editor.addEventListener('update', editorUpdate);
+}
+
+document.addEventListener('DOMContentLoaded', init);
